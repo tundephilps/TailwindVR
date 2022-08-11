06c2fddd87726cc9f6434c795bca2cc435d661a4
Tundephilps <tundephilps@gmail.com>
2022-08-11 11:17:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+// import components
+import Banner from './components/Banner';
+//import Header from './components/Header';
+//import NavMobile from './components/NavMobile';
+//import Experience from './components/Experience';
+//import Video from './components/Video';
+//import Headsets from './components/Headsets';
+//import Testimonial from './components/Testimonial';
+//import Explore from './components/Explore';
+
+// import aos (animate on scroll)
+//import Aos from 'aos';
+//import 'aos/dist/aos.css';
+
+
+
+
+function App () {
+    return (
+        
+            <div>
+            <Banner />
+            </div>
+    );
+}
+
+
+export default App;
